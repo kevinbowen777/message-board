ad85264cc3d972ee4f200ced8fbd77f403430ea4
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-01 10:08:33 -0700

    Add coverage for str repr

--- posts/tests.py
+++ posts/tests.py
@@ -6,12 +6,17 @@ from .models import Post
 
 class PostModelTest(TestCase):
     def setUp(self):
-        Post.objects.create(text="just a test")
+        self.post = Post.objects.create(text="just a test")
 
     def test_text_content(self):
         post = Post.objects.get(id=1)
-        expected_object_name = f"{post.text}"
-        self.assertEqual(expected_object_name, "just a test")
+        self.text = f"{post.text}"
+        # expected_object_name = f"{post.text}"
+        # self.assertEqual(expected_object_name, "just a test")
+        self.assertEqual(self.text, "just a test")
+
+    def test___str__(self):
+        self.assertEqual(str(self.post), self.post.text)
 
 
 class HomePageViewTest(TestCase):
