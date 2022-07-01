1f8f5e60462a1c8fd42037f91d8ed86c0390f944
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-01 14:31:48 -0700

    Add CreatePostView

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,18 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 from .models import Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = "posts/post_new.html"
+    fields = ("text", "author")
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 
 class HomePageView(ListView):
