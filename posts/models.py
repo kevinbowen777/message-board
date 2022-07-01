410edaaf38c8c69963ab28d0738726e9acbf7e41
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-01 14:14:26 -0700

    Add date & author to post. Fixes #35

--- posts/models.py
+++ posts/models.py
@@ -1,8 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
 class Post(models.Model):
     text = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        ordering = ["-date"]
 
     def __str__(self):
         return self.text[:50]
