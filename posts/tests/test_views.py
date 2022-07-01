06fb1996dc9e828f8918acd5a1ef44992c1f4504
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-01 11:59:02 -0700

    Move tests to separate modules. Fixes #25

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,28 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from .models import Post
-
-
-class PostModelTest(TestCase):
-    def setUp(self):
-        self.post = Post.objects.create(text="just a test")
-
-    def test_text_content(self):
-        post = Post.objects.get(id=1)
-        self.text = f"{post.text}"
-        # expected_object_name = f"{post.text}"
-        # self.assertEqual(expected_object_name, "just a test")
-        self.assertEqual(self.text, "just a test")
-
-    def test___str__(self):
-        self.assertEqual(str(self.post), self.post.text)
-
 
 class HomePageViewTest(TestCase):
-    def setUp(self):
-        Post.objects.create(text="this is another test")
-
     def test_view_url_exists_at_proper_location(self):
         resp = self.client.get("/")
         self.assertEqual(resp.status_code, 200)
