a44237f37dc814cc1f8fa48db041b6389bd45349
Kevin Bowen <kevin.bowen@gmail.com>
2022-07-01 17:30:49 -0700

    Create initial CreatePostView tests

--- posts/tests/test_views.py
+++ posts/tests/test_views.py
@@ -1,5 +1,35 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import RequestFactory, TestCase
 from django.urls import reverse
+
+from ..models import Post
+from ..views import PostCreateView
+
+factory = RequestFactory()
+
+
+class PostCreateViewTest(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="johndoe",
+            email="johndoe@example.com",
+            password="secret",
+        )
+
+        self.data = {
+            "text": "This is a post",
+            "author": self.user,
+        }
+        self.form = Post.objects.create(
+            text="Nice body content",
+            author=self.user,
+        )
+        request = factory.post("/new/", data=self.data)
+        request.user = self.user
+        response = PostCreateView.as_view()(request)  # noqa:F841
+        assert True
+        assert self.form.instance.author == self.request.user
+        assert self.form.is_valid()
 
 
 class HomePageViewTest(TestCase):
