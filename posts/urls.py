3da9bb645c3f556e7c20021ac6c72b9acf3f1fca
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-18 12:01:16 -0700

    Rename URLs

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import PostCreateView
 
 urlpatterns = [
-    path("messages/new/", PostCreateView.as_view(), name="post_new"),
+    path("messages/new/", PostCreateView.as_view(), name="message_new"),
 ]
