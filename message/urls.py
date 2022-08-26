93d92aef4fa83f3fe4b678b8e7e231c3cf04ef20
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-26 09:36:41 -0700

    Move message_list urlpattern back to message app

--- message/urls.py
+++ message/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from .views import MessageCreateView
+from .views import MessageCreateView, MessageListView
+
 
 urlpatterns = [
+    path("messages/", MessageListView.as_view(), name="message_list"),
     path("messages/new/", MessageCreateView.as_view(), name="message_new"),
 ]
--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from message.views import MessageListView
 
 from .views import AboutPageView, HomePageView
 
 urlpatterns = [
     path("about/", AboutPageView.as_view(), name="about"),
     path("", HomePageView.as_view(), name="home"),
-    path("messages/", MessageListView.as_view(), name="message_list"),
 ]
