bb89a005a37ac519a5dc8533ef636f0871a04f28
Kevin Bowen <kevin.bowen@gmail.com>
2022-08-26 09:10:47 -0700

    Add author to admin Message display. Fixes #71

--- message/admin.py
+++ message/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Message
 
-admin.site.register(Message)
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = (
+        "text",
+        "author",
+    )
+
+
+admin.site.register(Message, MessageAdmin)
