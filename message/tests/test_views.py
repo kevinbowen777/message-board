17588717656dd0131208cf4e1e3567ad85b96d60
Kevin Bowen <kevin.bowen@gmail.com>
2023-03-05 09:17:31 -0800

    Create new view tests for message actions

--- message/tests/test_views.py
+++ message/tests/test_views.py
@@ -1,13 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.test import RequestFactory, TestCase
+from django.test import TestCase
+from django.urls import reverse  # noqa:F401
 
 from ..models import Message
-from ..views import MessageCreateView
-
-factory = RequestFactory()
 
 
-class MessageCreateViewTest(TestCase):
+class MessageTests(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create_user(
             username="johndoe",
@@ -19,13 +17,44 @@ class MessageCreateViewTest(TestCase):
             "text": "This is a message",
             "author": self.user,
         }
-        self.form = Message.objects.create(
+        self.message = Message.objects.create(
             text="Nice body content",
             author=self.user,
         )
-        request = factory.message("/new/", data=self.data)
-        request.user = self.user
-        response = MessageCreateView.as_view()(request)  # noqa:F841
-        assert True
-        assert self.form.instance.author == self.request.user
-        assert self.form.is_valid()
+
+    def test_message_create_new(self):
+        self.client.login(email="johndoe@example.com", password="secret")
+        response = self.client.post(
+            reverse("message_new"),
+            {
+                "text": "New title",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Message.objects.first().text, "New title")
+
+    def test_message_update(self):
+        self.client.login(email="johndoe@example.com", password="secret")
+        response = self.client.post(
+            reverse("message_update", args={self.message.id}),
+            {
+                "text": "Updated title",
+            },
+        )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Message.objects.last().text, "Updated title")
+
+    def test_message_delete(self):
+        self.client.login(email="johndoe@example.com", password="secret")
+        response = self.client.post(
+            reverse("message_delete", args={self.message.id}),
+        )
+        self.assertEqual(response.status_code, 302)
+        # self.assertNotContains(Message.objects.all().text, "Updated title")
+
+    def test_message_list(self):
+        self.client.login(email="johndoe@example.com", password="secret")
+        response = self.client.get(reverse("message_list"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(Message.objects.last().text, "Nice body content")
+        self.assertTrue(self.message.author == self.user)
