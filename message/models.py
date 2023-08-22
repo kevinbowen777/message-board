a46330475769768ecbc3b971b118498a3712722f
Kevin Bowen <kevin.bowen@gmail.com>
2023-08-22 14:52:23 -0700

    Rename existing fields & add new dates, str, absolute_url(#260)

--- message/models.py
+++ message/models.py
@@ -1,21 +1,47 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 
 
 class Message(models.Model):
-    text = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
     author = models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE,
+        related_name="message_posts",
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.PUBLISHED,
     )
 
     class Meta:
-        ordering = ["-date"]
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
 
     def __str__(self):
-        return self.text[:50]
+        return self.title
 
+    """
     def get_absolute_url(self):
         return reverse("home")
+    """
+
+    def get_absolute_url(self):
+        return reverse(
+            "message_detail",
+            args=[self.publish.year, self.publish.month, self.publish.day, self.slug],
+        )
