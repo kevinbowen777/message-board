b0aef52ed154323900466bb7cd588364bc8897f2
Kevin Bowen <kevin.bowen@gmail.com>
2023-08-22 16:34:06 -0700

    Update layout & fields. Resolves #256

--- message/admin.py
+++ message/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from .models import Message
 
 
+@admin.register(Message)
 class MessageAdmin(admin.ModelAdmin):
-    list_display = (
-        "text",
-        "author",
-    )
-
-
-admin.site.register(Message, MessageAdmin)
+    list_display = ["title", "slug", "author", "publish", "status"]
+    list_filter = ["status", "created", "publish", "author"]
+    search_fields = ["title", "body"]
+    prepopulated_fields = {"slug": ("title",)}
+    raw_id_fields = ["author"]
+    date_hierarchy = "publish"
+    ordering = ["status", "publish"]
