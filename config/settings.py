083e35b746eb92100a5fc0076948c18cc874c95b
Kevin Bowen <kevin.bowen@gmail.com>
2023-10-04 18:58:27 -0700

    Update allauth. Add social providers config. Resolves #328 (#357)

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "allauth.account.middleware.AccountMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
